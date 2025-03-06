a8263e4b3732d3b3bdb464ba142ae88c1f672b2e
dabr00024 <padla00024@gmail.com>
2025-03-06 09:44:51 -0800

    add logic and changed colors

--- trafficlight/ViewController.swift
+++ trafficlight/ViewController.swift
@@ -7,6 +7,10 @@
 
 import UIKit
 
+enum lightNow {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet var redView: UIView!
@@ -15,11 +19,44 @@ class ViewController: UIViewController {
         
     @IBOutlet var startButton: UIButton!
     
+    private var ligftNow = lightNow.red
+    private let colorAlfaOn: CGFloat = 1
+    private let colorAlfaOff: CGFloat = 0.1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redView.alpha = colorAlfaOff
+        yellowView.alpha = colorAlfaOff
+        greenView.alpha = colorAlfaOff
+        
+        startButton.layer.cornerRadius = 10
     }
+    
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.width / 2
+        yellowView.layer.cornerRadius = yellowView.frame.width / 2
+        greenView.layer.cornerRadius = redView.frame.width / 2
+        
+    }
+
     @IBAction func startButtonDidTapped() {
+        
+     startButton.setTitle("NEXT", for: .normal)
+        
+        switch ligftNow {
+        case .red:
+            redView.alpha = colorAlfaOn
+            greenView.alpha = colorAlfaOff
+            ligftNow = .yellow
+        case .yellow:
+            yellowView.alpha = colorAlfaOn
+            redView.alpha = colorAlfaOff
+            ligftNow = .green
+        case .green:
+            greenView.alpha = colorAlfaOn
+            yellowView.alpha = colorAlfaOff
+            ligftNow = .red
+        }
     }
     
 
